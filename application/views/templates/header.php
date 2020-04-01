df2c7d7a7871530a95db1f1d76cf71b27934cfcc
Irfan Rafif <mirfanrafif17@gmail.com>
2020-04-01 18:11:46 +0700

    edit routing

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -93,13 +93,13 @@
               <a href="#" class="nav-link active">
                 <i class="nav-icon fas fa-tachometer-alt"></i>
                 <p>
-                  Dashboard
+                  Menu
                   <i class="right fas fa-angle-left"></i>
                 </p>
               </a>
               <ul class="nav nav-treeview">
                 <li class="nav-item">
-                  <a href="<?= base_url() ?>admin/home" class="nav-link">
+                  <a href="<?= base_url() ?>admin/dashboard" class="nav-link">
                     <i class="far fa-circle nav-icon"></i>
                     <p>Home</p>
                   </a>
